ad10385bc543739c2626f360416581851eaf2c9a
Sukruthi <sukruthipa@gamil.com>
2024-04-22 22:17:42 +0530

    dashboard

--- Pages_py/Dashboard.py
+++ Pages_py/Dashboard.py
@@ -4,10 +4,11 @@ from streamlit_option_menu import option_menu
 import Predict as p
 import login as log
 import Home as h
+from session_state import SessionState
 
 im = Image.open("favicon.ico")
 st.set_page_config(page_title="HRUDAY",page_icon=im,layout="wide")
-
+user_id = None
 with st.sidebar:
     selected=option_menu(
         menu_title="Menu",
@@ -15,6 +16,9 @@ with st.sidebar:
         icons=["box-arrow-in-right","house-fill","activity","clock-history","box-arrow-right"],
         default_index=0,
     )
+def process_data(user_id):
+    global user_id
+    user_id = userid
 
 if selected=="Account":
     log.login()
@@ -24,4 +28,6 @@ if selected=="Home":
 if selected=="Predict":
     p.predict()
 if selected=="History":
-    st.title(f"{selected}")        
+    st.title(f"{selected}")
+if selected=="Logout":
+    user_id = None
