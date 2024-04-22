2b939689ea99dc72ddd18601cd3f1bd83c24215f
Vagdevimn-0403 <vagdevimn@gmail.com>
2024-04-22 20:22:27 +0530

    local database connect

--- Pages_py/table_model.py
+++ Pages_py/table_model.py
@@ -4,5 +4,13 @@ connection = mysql.connector.connect(
     user=con.MYSQL_DATABASE_USER,
     password=con.MYSQL_DATABASE_PASSWORD,
     host=con.MYSQL_DATABASE_HOST,
-    database=con.MYSQL_DATABASE_DB
+    database=con.MYSQL_DATABASE_DB,
+    port=3308
 )
+
+cursor= connection.cursor()
+
+cursor.execute("SELECT * FROM user")
+data=cursor.fetchall()
+for x in data:
+  print(x)
